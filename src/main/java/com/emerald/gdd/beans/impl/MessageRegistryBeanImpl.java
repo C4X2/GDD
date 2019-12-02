65b26388f6775ee04ddbe26c28f05360c9e35a6d
C4X2 <nathaniel.brown.15@cnu.edu>
2019-12-01 20:50:21 -0500

    Spruced up index page

--- src/main/java/com/emerald/gdd/beans/impl/MessageRegistryBeanImpl.java
+++ src/main/java/com/emerald/gdd/beans/impl/MessageRegistryBeanImpl.java
@@ -10,24 +10,24 @@ import com.emerald.gdd.beans.model.MessageRegistryBean;
  * @author C4X2
  * @version 1.0
  */
-@ManagedBean(name="msg" , eager=true)
+@ManagedBean(name = "msg", eager = true)
 @ApplicationScoped()
 public class MessageRegistryBeanImpl implements MessageRegistryBean
 {
-	private final String trueName = "MessageResgistryBean";
-	
-	private String gddComment;
-	
+	private final String	trueName	= "MessageResgistryBean";
+
+	private String			gddComment;
+
 	public MessageRegistryBeanImpl()
 	{
-		
+
 	}
 
 	public String getTrueName()
 	{
 		return trueName;
 	}
-	
+
 	public String getGDDComment()
 	{
 		return "What is a GDD you ask?";
@@ -35,19 +35,20 @@ public class MessageRegistryBeanImpl implements MessageRegistryBean
 
 	public String getGddComment()
 	{
-		//return (gddComment == null) ? getGDDComment() : gddComment;
-		return "Hello World!";
+		return "A Game Design Document, or GDD, defines every aspect of your game. "
+				+ "Additionaly it acts as an orienting document for each stakeholder in your organization. "
+				+ "As a GDD explicitly states the goals game development needs to meet , from the small technical requirements to the large conceptual challenges. "
+				+ "Finally, A GDD is a resource for keeping a game true to its roots. If the game changes so should the GDD.";
 	}
 
 	public void setGddComment(String gddComment)
 	{
 		this.gddComment = gddComment;
 	}
-	
+
 	public String getHomepage()
 	{
 		return "Game Desgin Document";
 	}
-	
 
 }
