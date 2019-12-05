7e313f6fbe64fea775bd6fbebb0b729d9675cde8
C4X2 <nathaniel.brown.15@cnu.edu>
2019-12-04 23:03:29 -0500

    Finished front-end structure

--- src/main/java/com/emerald/gdd/beans/impl/GDDHeaderActionBeanImpl.java
+++ src/main/java/com/emerald/gdd/beans/impl/GDDHeaderActionBeanImpl.java
@@ -4,14 +4,9 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.model.SelectItem;
 
-@ManagedBean(name="headerActionBean")
+@ManagedBean(name="header")
 @SessionScoped
 public class GDDHeaderActionBeanImpl
 {
-	public static final String HOME = "Home";
 	
-	public SelectItem getHome()
-	{
-		return new SelectItem("Home", "home");
-	}
 }
